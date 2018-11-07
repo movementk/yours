9a31ff030f6102b70c6ef831f7279114e07037b9
kimyh5392 <mk.kimyh@gmail.com>
2018-11-07 18:32:01 +0900

    work 작업중 . . .

---
+++ work/work_list.php
@@ -0,0 +1,105 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php');?>
+<link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body class="sub work">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php');?>
+    
+    <main id="content">
+        <div class="container">
+            <div class="work-list">
+                <article class="list">
+                    <ul class="row">
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo01.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo02.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo03.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo04.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo05.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo06.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo07.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo08.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo09.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo10.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_logo11.jpg" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-4">
+                            <a href="#">
+                                <figure>
+                                    <img src="/assets/images/work/img_pop_design.png" class="img-fluid" alt="">
+                                </figure>
+                            </a>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </div>
+    </main>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php');?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php');?>
+</body>
+</html>
---
+++ work/work_view.php
@@ -0,0 +1,42 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php');?>
+<link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body class="work">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php');?>
+    
+    <main id="content">
+        <div class="container">
+            <article class="work-view">
+                <div class="article-header">
+                    <ul>
+                        <li>
+                            <dl>
+                                <dt>Client</dt>
+                                <dd>Yuhan-Kimberly</dd>
+                            </dl>
+                        </li>
+                        <li>
+                            <dl>
+                                <dt>Project</dt>
+                                <dd>좋은느낌 좋은순면 패키지 디자인</dd>
+                            </dl>
+                        </li>
+                    </ul>
+                </div>
+                <div class="article-content">
+                    <ul class="details-list">
+                        <li>
+                            <figure>
+                                <img src="" class="img-fluid" alt="">
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+        </div>
+    </main>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php');?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php');?>
+</body>
+</html>
